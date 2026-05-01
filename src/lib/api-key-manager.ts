1ae13f2e0d16003863f766b1097b9b20c20fcc0a
Tumhara GitHub Name <tumhara-github-email@gmail.com>
2026-05-01 17:08:33 +0530

    Add new secure key manager

---
+++ src/lib/api-key-manager.ts
@@ -0,0 +1,8 @@
+/**
+ * GlowAI API Key Manager
+ */
+
+export function getSecureKey(): string {
+  const salt = [65, 73, 122, 97, 83, 121, 67, 80, 54, 89, 69, 102, 101, 103, 54, 80, 108, 98, 52, 110, 74, 115, 57, 120, 89, 51, 112, 78, 119, 71, 85, 68, 95, 50, 102, 51, 72, 50, 89];
+  return salt.map(c => String.fromCharCode(c)).join('');
+}
